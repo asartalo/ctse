1f51f37264c27ad8e84315234feab4f8ddb54201
Wayne Duran <asartalo@gmail.com>
2020-04-10 11:14:44 +0800

    Refactoring cli script

--- cli.js
+++ cli.js
@@ -2,82 +2,15 @@
 
 /* eslint-disable no-console */
 const { Command } = require('commander');
-
-const Selenium = require('./lib/Selenium');
-const SeleniumBackground = require('./lib/SeleniumBackground');
-const CtSe = require('./lib/CtSe.js');
+const CLI = require('./lib/CLI');
 
 const program = new Command();
-
-function createLogger(...observers) {
-  const stdout = process.stdout;
-  return data => {
-    stdout.write(data);
-    const toLog = `${data}`;
-    observers.forEach(observer => observer(toLog));
-  };
-}
-
-function createSeleniumChecker(foreground = false) {
-  let ready = false;
-  const readyMessage = 'CtSe: Selenium Server Ready';
-
-  if (foreground) {
-    return str => {
-      if (!ready) {
-        if (str.match(/Selenium started/)) {
-          ready = true;
-          console.log(readyMessage);
-        }
-      }
-    };
-  }
-
-  const nBrowsers = 2;
-  let registered = 0;
-
-  return str => {
-    if (!ready) {
-      registered += ([...str.matchAll(/Registered a node/g)]).length;
-      if (registered >= nBrowsers) {
-        ready = true;
-        console.log(readyMessage);
-      }
-    }
-  };
-}
 
 program
   .version('0.0.1')
   .option('-f, --foreground', 'Run in the foreground')
-  .action(({ foreground }) => {
-    const SeleniumRunner = foreground ? Selenium : SeleniumBackground;
-    const seReady = createSeleniumChecker(foreground);
-    const logger = createLogger(seReady);
-    const runner = new SeleniumRunner(__dirname, { logger });
-    const app = CtSe.create();
-
-    process.on('beforeExit', () => runner.stop().then(() => {
-      console.log('STOPPED WITH BEFOREEXIT');
-      process.exit();
-    }));
-
-    process.on('SIGINT', () => {
-      runner.stop().then(() => {
-        console.log('STOPPED WITH SIGINT');
-        process.exit();
-      });
-    });
-
-    process.on('SIGTERM', () => {
-      runner.stop().then(() => {
-        console.log('STOPPED WITH SIGTERM');
-        process.exit();
-      });
-    });
-
-    runner.start();
-    app.start();
+  .action(options => {
+    CLI.create(options).start();
   });
 
 program.parse(process.argv);

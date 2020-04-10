4d00a3f73ad05fe1ef2a40abc33d8877497a5108
Wayne Duran <asartalo@gmail.com>
2020-04-10 19:14:09 +0800

    Test for CLI#start()

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,3 +1,10 @@
+const chai = require('chai');
+const dirtyChai = require('dirty-chai');
+const sinonChai = require('sinon-chai');
+
+chai.use(dirtyChai);
+chai.use(sinonChai);
+
 /* global before */
 /*
  eslint mocha/no-top-level-hooks: "off", mocha/no-hooks-for-single-case: "off"

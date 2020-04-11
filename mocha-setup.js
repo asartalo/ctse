73ff95f9d6c03dea9dd237f450346c6eb3b970f8
Wayne Duran <asartalo@gmail.com>
2020-04-11 12:33:31 +0800

    Adding tests to shellRunner

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,8 +1,10 @@
 const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 const sinonChai = require('sinon-chai');
+const chaiAsPromised = require('chai-as-promised');
 
 chai.use(dirtyChai);
+chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
 /* global before */

e93d23af8512fc0362c427b6bed55a24247231ba
Wayne Duran <asartalo@gmail.com>
2020-05-17 19:20:16 +0800

    feat: add --no-se cli flag
    Allows CtSe to run without Selenium. Mainly for testing purposes.

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ const program = new Command();
 program
   .version('0.0.1')
   .option('-f, --foreground', 'Run in the foreground')
+  .option('-S, --no-se', 'No selenium')
   .action(options => CLI.create(options).start());
 
 program.parse(process.argv);
